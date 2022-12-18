11ed71a326e93713970217cd1eb3d14880b7199c
Lukas Sommer <sommerluk@gmail.com>
2022-12-18 11:38:41 +0000

    Translation update

--- lib/translation/perceptualcolorlib.de.ts
+++ lib/translation/perceptualcolorlib.de.ts
@@ -4,13 +4,13 @@
 <context>
     <name>PerceptualColor::ColorDialogPrivate</name>
     <message>
-        <location filename="../src/colordialog.cpp" line="501"/>
+        <location filename="../src/colordialog.cpp" line="502"/>
         <source>Lightness</source>
         <extracomment>@label:slider Accessible name for lightness slider. This is different from “brightness”/“value” and should therefore get a different translation.</extracomment>
         <translation>Helligkeit</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="400"/>
+        <location filename="../src/colordialog.cpp" line="401"/>
         <source>&amp;Basic colors</source>
         <extracomment>@title:tab Same text as in QColorDialog</extracomment>
         <translation>Gru&amp;ndfarben</translation>
@@ -81,43 +81,43 @@
         <translation type="vanished">Farbraumumwandlungs-Profil</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="256"/>
+        <location filename="../src/colordialog.cpp" line="257"/>
         <source>Color model:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. The color model of the color space which is described by this profile.</extracomment>
         <translation>Farbmodell:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="274"/>
+        <location filename="../src/colordialog.cpp" line="275"/>
         <source>Device model:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. This is usually the model identifier of the device to which the colour profile applies.</extracomment>
         <translation>Gerätemodell:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="340"/>
+        <location filename="../src/colordialog.cpp" line="341"/>
         <source>PCS color model:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. The color model of the PCS (profile connection space) which is used internally by this profile.</extracomment>
         <translation>PCS-Farbmodell:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="379"/>
+        <location filename="../src/colordialog.cpp" line="380"/>
         <source>Color space information</source>
         <extracomment>@info:intext Title of info text about current color space (will be followed by other information as available in the color profile.</extracomment>
         <translation>Farbraum-Informationen</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="385"/>
+        <location filename="../src/colordialog.cpp" line="386"/>
         <source>CIEHL&amp;C D50:</source>
         <extracomment>@label:spinbox Label for CIE’s CIEHLC color model, based on Hue, Lightness, Chroma, and using the D50 illuminant as white point.</extracomment>
         <translation>CIE-HL&amp;C D50:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="405"/>
+        <location filename="../src/colordialog.cpp" line="406"/>
         <source>&amp;Hue-based</source>
         <extracomment>@title:tab</extracomment>
         <translation>Nach Farb&amp;ton</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="412"/>
+        <location filename="../src/colordialog.cpp" line="413"/>
         <source>&amp;Lightness-based</source>
         <extracomment>@title:tab “lightness” is different from “brightness”/“value” and should therefore get a different translation.</extracomment>
         <translation>Nach &amp;Helligkeit</translation>
@@ -129,13 +129,13 @@
         <translation>%</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="527"/>
+        <location filename="../src/colordialog.cpp" line="528"/>
         <source>O&amp;pacity:</source>
         <extracomment>@label:slider An opacity of 0 means completely transparent. The higher the opacity value increases, the more opaque the colour becomes, until it finally becomes completely opaque at the highest possible opacity value.</extracomment>
         <translation>&amp;Deckkraft:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="441"/>
+        <location filename="../src/colordialog.cpp" line="442"/>
         <source>&amp;Cancel</source>
         <extracomment>@action:button</extracomment>
         <translation>&amp;Abbrechen</translation>
@@ -196,145 +196,145 @@
         <translation type="vanished">Ausgabe-Profil</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="247"/>
+        <location filename="../src/colordialog.cpp" line="248"/>
         <source>Profile class:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space.</extracomment>
         <translation>Profiltyp:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="265"/>
+        <location filename="../src/colordialog.cpp" line="266"/>
         <source>Manufacturer:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. This is usually the manufacturer of the device to which the colour profile applies.</extracomment>
         <translation>Hersteller:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="288"/>
+        <location filename="../src/colordialog.cpp" line="289"/>
         <source>Created:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. This is the date and time of the creation of the profile.</extracomment>
         <translation>Erstellt:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="297"/>
+        <location filename="../src/colordialog.cpp" line="298"/>
         <source>ICC format:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. This is the version number of the ICC file format that is used.</extracomment>
         <translation>ICC-Format:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="309"/>
+        <location filename="../src/colordialog.cpp" line="310"/>
         <source>Implementation:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. Wether the profile has a matrix shaper or a color lookup table (CLUT) or both.</extracomment>
         <translation>Umsetzung:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="316"/>
+        <location filename="../src/colordialog.cpp" line="317"/>
         <source>Matrices and color lookup tables</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. Wether the profile has a matrix shaper or a color lookup table (CLUT) or both.</extracomment>
         <translation>Matrizen und Farbzuordnungstabellen</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="322"/>
+        <location filename="../src/colordialog.cpp" line="323"/>
         <source>Matrices</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. Wether the profile has a matrix shaper or a color lookup table (CLUT) or both.</extracomment>
         <translation>Matrizen</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="329"/>
+        <location filename="../src/colordialog.cpp" line="330"/>
         <source>Color lookup tables</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. Wether the profile has a matrix shaper or a color lookup table (CLUT) or both.</extracomment>
         <translation>Farbzuordnungstabellen</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="347"/>
+        <location filename="../src/colordialog.cpp" line="348"/>
         <source>Copyright:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. The copyright of this profile.</extracomment>
         <translation>Urheberrecht:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="355"/>
+        <location filename="../src/colordialog.cpp" line="356"/>
         <source>File size:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space. This is the size of the ICC file that was read in.</extracomment>
         <translation>Dateigröße:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="363"/>
+        <location filename="../src/colordialog.cpp" line="364"/>
         <source>File name:</source>
         <extracomment>@item:intext An information from the color profile to be added to the info text about current color space.</extracomment>
         <translation>Dateiname:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="389"/>
+        <location filename="../src/colordialog.cpp" line="390"/>
         <source>Oklch:</source>
         <extracomment>@label:spinbox Label for Oklch color model, based on Lightness, Chroma, * Hue, and using the D65 illuminant as white point.</extracomment>
         <translation>Oklch:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="428"/>
+        <location filename="../src/colordialog.cpp" line="429"/>
         <source>HS&amp;L:</source>
         <extracomment>@label:spinbox HSL (hue, saturation, lightness)</extracomment>
         <translation>HS&amp;L:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="432"/>
+        <location filename="../src/colordialog.cpp" line="433"/>
         <source>HS&amp;V/HSB:</source>
         <extracomment>@label:spinbox HSV (hue, saturation, value) and HSB (hue, saturation, brightness) are two different names for the very same color model.</extracomment>
         <translation>HS&amp;V/HSB:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="435"/>
+        <location filename="../src/colordialog.cpp" line="436"/>
         <source>H&amp;WB:</source>
         <extracomment>@label:spinbox HWB (hue, whiteness, blackness)</extracomment>
         <translation>H&amp;WB:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="438"/>
+        <location filename="../src/colordialog.cpp" line="439"/>
         <source>&amp;OK</source>
         <extracomment>@action:button</extracomment>
         <translation>&amp;OK</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="445"/>
+        <location filename="../src/colordialog.cpp" line="446"/>
         <source>&lt;p&gt;Red: 0⁠–⁠255&lt;/p&gt;&lt;p&gt;Green: 0⁠–⁠255&lt;/p&gt;&lt;p&gt;Blue: 0⁠–⁠255&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for RGB spinbox.</extracomment>
         <translation>&lt;p&gt;Rot: 0⁠–⁠255&lt;/p&gt;&lt;p&gt;Grün: 0⁠–⁠255&lt;/p&gt;&lt;p&gt;Blau: 0⁠–⁠255&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="452"/>
+        <location filename="../src/colordialog.cpp" line="453"/>
         <source>&lt;p&gt;Hexadecimal color code, as used in HTML: #RRGGBB&lt;/p&gt;&lt;p&gt;RR: two-digit code for red: 00⁠–⁠FF&lt;/p&gt;&lt;p&gt;GG: two-digit code for green: 00⁠–⁠FF&lt;/p&gt;&lt;p&gt;BB: two-digit code for blue: 00⁠–⁠FF&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for hexadecimal code.</extracomment>
         <translation>&lt;p&gt;Hexadezimaler Farbcode, wie in HTML: #RRGGBB&lt;/p&gt;&lt;p&gt;RR: zwei Ziffern für rot: 00⁠–⁠FF&lt;/p&gt;&lt;p&gt;GG: zwei Ziffern für grün: 00⁠–⁠FF&lt;/p&gt;&lt;p&gt;BB: zwei Ziffern für blau: 00⁠–⁠FF&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="506"/>
+        <location filename="../src/colordialog.cpp" line="507"/>
         <source>&lt;p&gt;Hue: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;Lightness: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Chroma: 0⁠–⁠%L1&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for CIEHLC. “lightness” is different from “brightness”/“value” and should therefore get a different translation.</extracomment>
         <translation>&lt;p&gt;Farbton: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;Helligkeit: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Buntheit: 0⁠–⁠%L1&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="515"/>
+        <location filename="../src/colordialog.cpp" line="516"/>
         <source>&lt;p&gt;Lightness: %L1⁠–⁠%L2&lt;/p&gt;&lt;p&gt;Chroma: %L3⁠–⁠%L4&lt;/p&gt;&lt;p&gt;Hue: 0°⁠–⁠360°&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for Oklch. “lightness” is different from “brightness”/“value” and should therefore get a different translation.</extracomment>
         <translation>&lt;p&gt;Helligkeit: %L1⁠–⁠%L2&lt;/p&gt;&lt;p&gt;Buntheit: %L3⁠–⁠%L4&lt;/p&gt;&lt;p&gt;Farbton: 0°⁠–⁠360°&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="465"/>
+        <location filename="../src/colordialog.cpp" line="466"/>
         <source>&lt;p&gt;Hue: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;HSL-Saturation: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Lightness: 0%⁠–⁠100%&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for HSL (hue, saturation, lightness). Saturation: 0 means something on the grey axis; 255 means something between the grey axis and the most colorful color. This is different from “chroma” and should therefore get a different translation. Lightness: 0 means always black; 255 means always white. This is different from “brightness” and should therefore get a different translation.</extracomment>
         <translation>&lt;p&gt;Farbton: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;HSL-Sättigung: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Helligkeit: 0%⁠–⁠100%&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="418"/>
+        <location filename="../src/colordialog.cpp" line="419"/>
         <source>From &amp;screen</source>
         <extracomment>@title:tab</extracomment>
         <translation>&amp;Pipette</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="479"/>
+        <location filename="../src/colordialog.cpp" line="480"/>
         <source>&lt;p&gt;Hue: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;Whiteness: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Blackness: 0%⁠–⁠100%&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for HWB (hue, whiteness, blackness). The idea behind is that the hue defines the pure (maximum colorful) color. Than, white color can be added, creating a “tint”. Or black color can be added, creating a “shade”. Or both can be added, creating a “tone“. See https://en.wikipedia.org/wiki/Tint,_shade_and_tone for more information. 0% white + 0% black = pure color. 100% white + 0% black = white. 0% white + 100% black = black. 50% white + 50% black = gray. 50% white + 0% black = tint. 25% white + 25% black = tone. 0% white + 50% black = shade.</extracomment>
         <translation>&lt;p&gt;Farbton: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;Weißbeimischung: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Schwarzbeimischung: 0%⁠–⁠100%&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="492"/>
+        <location filename="../src/colordialog.cpp" line="493"/>
         <source>&lt;p&gt;Hue: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;HSV/HSB-Saturation: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Brightness/Value: 0%⁠–⁠100%&lt;/p&gt;</source>
         <extracomment>@info:tooltip Help text for HSV/HSB. HSV (hue, saturation, value) and HSB (hue, saturation, brightness) are two different names for the very same color model. Saturation: 0 means something between black and white; 255 means something between black and the most colorful color. This is different from “chroma” and should therefore get a different translation. Brightness/value: 0 means always black; 255 means something between white and the most colorful color. This is different from “lightness” and should therefore get a different translation.</extracomment>
         <translation>&lt;p&gt;Farbton: 0°⁠–⁠360°&lt;/p&gt;&lt;p&gt;HSV/HSB-Sättigung: 0%⁠–⁠100%&lt;/p&gt;&lt;p&gt;Strahlkraft: 0%⁠–⁠100%&lt;/p&gt;</translation>
@@ -346,31 +346,31 @@
         <translation>°</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="617"/>
+        <location filename="../src/colordialog.cpp" line="618"/>
         <source>&amp;Pick Screen Color</source>
         <extracomment>@action:button Same text as in QColorDialog</extracomment>
         <translation>&amp;Farbe vom Bildschirm wählen</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="1354"/>
+        <location filename="../src/colordialog.cpp" line="1355"/>
         <source>Select color</source>
         <extracomment>@title:window Default window title. Same text as in QColorDialog</extracomment>
         <translation>Farbauswahl</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="392"/>
+        <location filename="../src/colordialog.cpp" line="393"/>
         <source>&amp;RGB:</source>
         <extracomment>@label:spinbox Label for RGB color model, based on Red, Green, Blue.</extracomment>
         <translation>&amp;RGB:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="395"/>
+        <location filename="../src/colordialog.cpp" line="396"/>
         <source>He&amp;x:</source>
         <extracomment>@label:textbox Label for hexadecimal RGB representation like #12ab45</extracomment>
         <translation>He&amp;x:</translation>
     </message>
     <message>
-        <location filename="../src/colordialog.cpp" line="424"/>
+        <location filename="../src/colordialog.cpp" line="425"/>
         <source>&amp;Numeric</source>
         <extracomment>@title:tab</extracomment>
         <translation>&amp;Numerisch</translation>
