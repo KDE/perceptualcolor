850260bc75838ff0bf527964febd2927d71b6f8c
Lukas Sommer <sommerluk@gmail.com>
2023-01-15 15:49:10 +0100

    Detailed warnings for both, Qt5 and Qt6 build

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -49,7 +49,7 @@ rm artifact_doxygen_temp
 rm --recursive --force doxyconf
 [ -s artifact_doxygen ] || ((errorcount++))
 
-echo Build with warnings started.
+echo Build with warnings against Qt5 started.
 # The “.” command will execute the given script within the context of
 # the current script, which is necessary in order to preserve the
 # environment variables that are set by the given script.
@@ -64,11 +64,34 @@ cmake \
     -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=/usr/bin/iwyu \
         -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=FALSE \
     -DADDITIONAL_WARNINGS=TRUE \
+    -DBUILD_WITH_QT6=OFF \
     ..
-cmake --build . --parallel $PARALLEL_PROCESSES 2>../artifact_warnings
+cmake --build . --parallel $PARALLEL_PROCESSES 2>../artifact_warnings_qt5
 cd ..
 [ -s artifact_warnings ] || ((errorcount++))
-echo Build with warnings finished
+echo Build with warnings against Qt5 finished
+
+echo Build with warnings against Qt6 started.
+# The “.” command will execute the given script within the context of
+# the current script, which is necessary in order to preserve the
+# environment variables that are set by the given script.
+. scripts/export-environment.sh
+echo Number of available CPU threads: $PARALLEL_PROCESSES
+mkdir --parents build
+rm --recursive --force build/*
+cd build
+cmake \
+    -DCMAKE_CXX_COMPILER=clazy \
+    -DCMAKE_CXX_CLANG_TIDY=/usr/bin/clang-tidy \
+    -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=/usr/bin/iwyu \
+        -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=FALSE \
+    -DADDITIONAL_WARNINGS=TRUE \
+    -DBUILD_WITH_QT6=ON \
+    ..
+cmake --build . --parallel $PARALLEL_PROCESSES 2>../artifact_warnings_qt6
+cd ..
+[ -s artifact_warnings ] || ((errorcount++))
+echo Build with warnings against Qt6 finished
 
 scripts/automatic-integration.sh
 git diff > artifact_automatic_integration_diff
