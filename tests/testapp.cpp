d8c5cb59333763cdf485b324a22dab308342fc18
Lukas Sommer <sommerluk@gmail.com>
2023-05-16 10:42:34 +0000

    Fallback profile for testapp if requested profile is missing

--- tests/testapp.cpp
+++ tests/testapp.cpp
@@ -103,6 +103,9 @@ int main(int argc, char *argv[])
             // QStringLiteral("/usr/share/color/icc/krita/ITUR_2100_PQ_FULL.ICC") //
             // QStringLiteral("/usr/share/color/icc/ECI-RGB.V1.0.icc") //
         );
+    if (myColorSpace.isNull()) {
+        myColorSpace = PerceptualColor::RgbColorSpaceFactory::createSrgb();
+    }
 
     PerceptualColor::ColorDialog m_colorDialog(myColorSpace);
     // For session management, according to https://doc.qt.io/qt-6/session.html
