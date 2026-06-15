b525ed7a87cdc505af24b82b8bc3ba20bfa81838
Lukas Sommer <sommerluk@gmail.com>
2026-06-15 10:53:53 +0000

    Fix broken first color opacity in gradients

--- src/gradientimageparameters.cpp
+++ src/gradientimageparameters.cpp
@@ -77,6 +77,7 @@ void GradientImageParameters::setFirstColorLchA(const GenericColor &newFirstColo
     }
 
     m_firstColorLchCorrected = correctedNewFirstColorLch;
+    m_firstColorAlphaCorrected = correctedNewFirstColorAlpha;
     updateSecondColor();
 }
 
